4849bc443c41963ed0e74e42b64adc7f785623da
redplusblue <samirkabra1161@gmail.com>
2024-08-04 05:26:28 -0400

    Updated register component with picsum links for images

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -61,8 +61,7 @@ export default function Register() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage:
-              "url(https://source.unsplash.com/random?wallpapers)",
+            backgroundImage: "url(https://picsum.photos/1280/720?grayscale)",
             backgroundRepeat: "no-repeat",
             backgroundColor: (t) =>
               t.palette.mode === "light"
