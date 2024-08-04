243c40fbfc8bef84fafceb116862efbfa3e716ca
redplusblue <samirkabra1161@gmail.com>
2024-08-04 05:10:39 -0400

    updated config with base for the repo x2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/phantom-portfolio/",
+  base: "https://redplusblue.github.io/phantom-portfolio/",
   ssr: {
     external: ["react", "react-dom"],
   },
